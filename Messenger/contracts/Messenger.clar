46d241d51b2f1efc0996e2074ad0932467abbd7a
Ummar Ikram <59392080+ummarikram@users.noreply.github.com>
2021-08-13 17:26:11 +0500

    Fixed Bugs & Added Working Test

--- Messenger/contracts/Messenger.clar
+++ Messenger/contracts/Messenger.clar
@@ -1,37 +1,34 @@
-;; For testing
-;; (define-map Messages-map { sender: principal } { message:  (list 50 (string-ascii 100)) } )
-
 ;; Composite key Approach
-(define-map Messages-map { users: { sender: principal, reciever: principal } } { message:  (list 50 (string-ascii 100)) } )
+(define-map Messages-map { usernames: { sender: (string-ascii 10), reciever: (string-ascii 10) } } { message:  (list 50 (string-ascii 100)) } )
 
 ;; Default message for first-time
-(define-data-var default (list 50 (string-ascii 100)) (list "Say Hi!"))
+(define-data-var default (list 50 (string-ascii 100)) (list ""))
  
-(define-public (send-message (reciever principal) (text (string-ascii 100)))
+(define-public (send-message (sender (string-ascii 10)) (reciever (string-ascii 10)) (text (string-ascii 100)))
     (let 
         (
             (first-message (var-get default))
 
             ;; if sending message for the first time then default to first-message else get list of previously sent messages
-            (messages (default-to first-message (get message (map-get? Messages-map { users: { sender: tx-sender, reciever: reciever } }))))
+            (messages (default-to first-message (get message (map-get? Messages-map { usernames: { sender: sender, reciever: reciever } }))))
         )
         
-        ;;(ok (print messages))
+        ;; (print messages)
         
-        (ok (map-insert Messages-map { users: { sender: tx-sender, reciever: reciever } } { message: (unwrap-panic (as-max-len? (append messages text) u50)) } ))
+        (ok (map-set Messages-map { usernames: { sender: sender, reciever: reciever } } { message: (unwrap-panic (as-max-len? (append messages text) u50)) } ))
         
     )
 )
 
 
-(define-public (get-sent-messages (reciever principal))
+(define-public (get-sent-messages (sender (string-ascii 10)) (reciever (string-ascii 10)))
     (let 
 
         (
-            (messages (get message (unwrap! (map-get? Messages-map { users: { sender: tx-sender, reciever: reciever } }) (err (var-get default)))))
+            (messages (get message (unwrap! (map-get? Messages-map { usernames: { sender: sender, reciever: reciever } }) (err (var-get default)))))
         )
         
-        ;;(ok (print messages))
+        ;; (print messages)
         
         ;; return list of messages between the two users where the calling user sent the message.
         (ok messages)
@@ -40,14 +37,14 @@
 )
 
 
-(define-public (get-recieved-messages (sender principal))
+(define-public (get-recieved-messages (sender (string-ascii 10)) (reciever (string-ascii 10)))
     (let 
     
         (
-            (messages (get message (unwrap! (map-get? Messages-map { users: { sender: sender, reciever: tx-sender } }) (err (var-get default)))))
+            (messages (get message (unwrap! (map-get? Messages-map { usernames: { sender: sender, reciever: reciever } }) (err (var-get default)))))
         )
         
-        ;;(ok (print messages))
+        ;; (print messages)
         
         ;; return list of messages between the two users where the calling user recieved the message. 
         (ok messages)
--- Messenger/tests/Messenger_test.ts
+++ Messenger/tests/Messenger_test.ts
@@ -3,24 +3,61 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
+    name: "Ensure that sending & recieving works",
     async fn(chain: Chain, accounts: Map<string, Account>) {
+        let wallet_1 = accounts.get('wallet_1')!;
         let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
+            Tx.contractCall('Messenger', 'get-sent-messages', [types.ascii("ummar"), types.ascii("ahmed")], wallet_1.address),
+            Tx.contractCall('Messenger', 'send-message', [types.ascii("ummar"), types.ascii("ahmed"), types.ascii("Hello Ahmed")], wallet_1.address),
+            Tx.contractCall('Messenger', 'get-sent-messages', [types.ascii("ummar"), types.ascii("ahmed")], wallet_1.address),
+            Tx.contractCall('Messenger', 'send-message', [types.ascii("ummar"), types.ascii("ahmed"), types.ascii("How are you?")], wallet_1.address),
+            Tx.contractCall('Messenger', 'get-sent-messages', [types.ascii("ummar"), types.ascii("ahmed")], wallet_1.address),
+            Tx.contractCall('Messenger', 'send-message', [types.ascii("ahmed"), types.ascii("ummar"), types.ascii("I am fine")], wallet_1.address),
+            Tx.contractCall('Messenger', 'get-recieved-messages', [types.ascii("ummar"), types.ascii("ahmed")], wallet_1.address),
+            Tx.contractCall('Messenger', 'get-recieved-messages', [types.ascii("ahmed"), types.ascii("ummar")], wallet_1.address),
         ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
 
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
+        assertEquals(block.receipts.length, 8);
+        assertEquals(block.height, 2);
+        
+        // No message was sent between the two users
+        block.receipts[0].result
+        .expectErr()
+        .expectList()
+
+        // Message sent successfully  ummar->ahmed
+        block.receipts[1].result
+        .expectOk()
+        .expectBool(true)
+        
+        // Expect Ok and a List since a message was sent previously by ummar
+        block.receipts[2].result
+        .expectOk()
+        .expectList()
+
+        // Message sent successfully  ummar->ahmed
+        block.receipts[3].result
+        .expectOk()
+        .expectBool(true)
+
+        // Expect Ok and a List since two messages were sent previously by ummar
+        block.receipts[4].result
+        .expectOk()
+        .expectList()
+        
+        // Message sent successfully  ahmed->ummar
+        block.receipts[5].result
+        .expectOk()
+        .expectBool(true)
+
+        // Expect Ok and a List since a message was recieved by ummar previously
+        block.receipts[6].result
+        .expectOk()
+        .expectList()
+
+        // Expect Ok and a List since two messages were recieved by ahmed previously
+        block.receipts[7].result
+        .expectOk()
+        .expectList()
     },
 });
